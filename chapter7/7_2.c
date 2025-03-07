ee7a887d8ab3d959788d64747dadbb899425f7fb
econaz <563380805@qq.com>
2025-03-07 20:54:51 +0800

    add 7_2.c

--- chapter7/7_2.c
+++ chapter7/7_2.c
@@ -1,30 +1,75 @@
 
 
-// #include <stddef.h>
-// #include <unistd.h>
-//
-// struct block {
-//   _Bool free;
-//   size_t size;
-//   struct block *next;
-// };
-//
-// typedef struct block bk;
-// bk *global = NULL;
-//
-// void *my_malloc(size_t size) {
-//
-//   bk *ptr = global;
-//   if (ptr) {
-//
-//   } else {
-//     bk *block = sbrk(0);
-//     bk *next = sbrk(sizeof(bk) + size);
-//     if (next == -1) {
-//       return NULL;
-//     }
-//     block->size = size;
-//     block->next = NULL;
-//     block->free = TRUE;
-//   }
-// }
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+struct block {
+  int free;
+  size_t size;
+  struct block *next;
+};
+
+typedef struct block bk;
+bk *global = NULL;
+
+void *my_malloc(size_t size) {
+
+  bk *ptr = global;
+  bk *pre = NULL;
+  bk *temp = NULL;
+  while (ptr && !(ptr->free && ptr->size >= size)) {
+    pre = ptr;
+    ptr = ptr->next;
+  }
+
+  if (ptr == NULL) {
+    ptr = (bk *)sbrk(0);
+    if (sbrk(sizeof(bk) + size) == (void *)-1)
+      return NULL;
+
+    ptr->size = size;
+    ptr->next = NULL;
+    ptr->free = 0;
+
+    if (pre) {
+      pre->next = ptr;
+    } else {
+      global = ptr;
+    }
+    return ptr + sizeof(bk);
+  }
+
+  if (ptr->size - size >= sizeof(bk)) {
+    temp = ptr;
+    temp->size = ptr->size - size - sizeof(bk);
+    temp->next = ptr->next;
+    temp->free = 1;
+    ptr->size = size;
+    ptr->next = temp;
+    return ptr + sizeof(bk);
+  } else {
+    temp = (bk *)sbrk(0);
+
+    if (sbrk(sizeof(bk) + size) == (void *)-1)
+      return NULL;
+
+    temp->size = size;
+    temp->next = ptr;
+    pre->next = temp;
+    return ptr + sizeof(bk);
+  }
+  return NULL;
+}
+int main(int argc, char *argv[]) {
+  printf("%10p\n", sbrk(0));
+  printf("%lu\n", sizeof(bk));
+  sbrk(1999);
+  printf("%10p\n", sbrk(0));
+  for (int i = 0; i < 10; i++) {
+    char *ptr = (char *)my_malloc(1);
+    printf("%10p\n", sbrk(0));
+  }
+  printf("%10p\n", sbrk(0));
+}
