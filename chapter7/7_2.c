15aedbeb02057157eb084c876deaadffbb481a83
econaz <563380805@qq.com>
2025-03-08 20:47:57 +0800

    modify 7_2.c

--- chapter7/7_2.c
+++ chapter7/7_2.c
@@ -13,7 +13,35 @@ struct block {
 
 typedef struct block bk;
 bk *global = NULL;
+bk *split(bk *pre, bk *ptr, size_t size) {
+  bk *temp;
+  long inside;
+  long outsize;
+  ptr = (bk *)sbrk(0);
+  if (size < sizeof(bk))
+    inside = 4 * sizeof(bk);
+  else
+    inside = 2 * (sizeof(bk) + size);
+  if (sbrk(inside) == (void *)-1)
+    return NULL;
 
+  ptr->size = size;
+  ptr->free = 0;
+
+  outsize = size + sizeof(bk);
+  temp = (bk *)((char *)ptr + outsize);
+  temp->size = inside - sizeof(bk) - size;
+  temp->free = 1;
+  temp->next = NULL;
+
+  ptr->next = temp;
+  if (pre) {
+    pre->next = ptr;
+  } else {
+    global = ptr;
+  }
+  return ptr;
+}
 void *my_malloc(size_t size) {
 
   bk *ptr = global;
@@ -23,53 +51,66 @@ void *my_malloc(size_t size) {
     pre = ptr;
     ptr = ptr->next;
   }
-
   if (ptr == NULL) {
-    ptr = (bk *)sbrk(0);
-    if (sbrk(sizeof(bk) + size) == (void *)-1)
+    printf("ener ptr = null\n");
+    ptr = split(pre, ptr, size);
+    if (ptr == NULL)
       return NULL;
 
-    ptr->size = size;
-    ptr->next = NULL;
-    ptr->free = 0;
-
-    if (pre) {
-      pre->next = ptr;
-    } else {
-      global = ptr;
-    }
-    return ptr + sizeof(bk);
+    return ptr + 1;
   }
 
   if (ptr->size - size >= sizeof(bk)) {
-    temp = ptr;
-    temp->size = ptr->size - size - sizeof(bk);
-    temp->next = ptr->next;
-    temp->free = 1;
-    ptr->size = size;
-    ptr->next = temp;
-    return ptr + sizeof(bk);
+    if (ptr->size - size == sizeof(bk)) {
+      printf("enter size == sizeof(bk)\n");
+      ptr->free = 0;
+      ptr->next = NULL;
+      ptr->size = size;
+    } else {
+      printf("enter size > sizeof(bk)\n");
+
+      long free_size = ptr->size - size - sizeof(bk);
+      if (free_size < sizeof(bk)) {
+        temp = NULL;
+        if ((void *)-1 == sbrk(-free_size))
+          return NULL;
+
+      } else {
+        temp = (bk *)((char *)ptr + size + sizeof(bk));
+        temp->free = 1;
+        temp->next = NULL;
+        temp->size = free_size;
+      }
+      ptr->free = 0;
+      ptr->size = size;
+      ptr->next = temp;
+    }
+    return ptr + 1;
   } else {
-    temp = (bk *)sbrk(0);
-
-    if (sbrk(sizeof(bk) + size) == (void *)-1)
+    printf("enter size < sizeof\n");
+    ptr = split(pre, ptr, size);
+    if (ptr == NULL)
       return NULL;
-
-    temp->size = size;
-    temp->next = ptr;
-    pre->next = temp;
-    return ptr + sizeof(bk);
+    return ptr + 1;
   }
   return NULL;
 }
 int main(int argc, char *argv[]) {
+  // printf("%10p\n", sbrk(0));
+  // printf("%lu\n", sizeof(bk));
+  // sbrk(1999);
   printf("%10p\n", sbrk(0));
-  printf("%lu\n", sizeof(bk));
-  sbrk(1999);
-  printf("%10p\n", sbrk(0));
-  for (int i = 0; i < 10; i++) {
-    char *ptr = (char *)my_malloc(1);
+  char *ptr = (char *)my_malloc(200);
+  ptr = "test";
+  for (int i = 0; i < 5; i++) {
+    char *ptr1 = (char *)my_malloc(50);
     printf("%10p\n", sbrk(0));
   }
-  printf("%10p\n", sbrk(0));
+
+  for (int i = 0; i < 6; i++) {
+    ptr = (char *)my_malloc(1);
+    printf("%10p\n", sbrk(0));
+    if (i == 5)
+      printf(" i == 10 %10p\n", ptr);
+  }
 }
