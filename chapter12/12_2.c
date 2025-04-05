d6443327632fcffad864ea8a6f478e1c3135e143
econaz <econazx@gmail.com>
2025-04-05 19:46:28 +0800

    modify 12_2.c

--- chapter12/12_2.c
+++ chapter12/12_2.c
@@ -15,6 +15,15 @@ struct Node {
   struct Node *brother;
   struct Node *child;
 };
+struct status {
+  pid_t pid;
+  pid_t ppid;
+  char *names;
+};
+
+struct Node *head = NULL;
+
+void initHead() { head = (struct Node *)malloc(sizeof(struct Node)); }
 
 struct Node *buildNode(char *names, pid_t pid, struct Node *brother,
                        struct Node *child) {
@@ -27,9 +36,9 @@ struct Node *buildNode(char *names, pid_t pid, struct Node *brother,
   re->child = child;
   return re;
 }
-void printTree(struct Node *head, int size) {
+void printTree(struct Node *head, int size, char *flag) {
 
-  //\u250c |_ 252c --| 2514   251c
+  // 250c |_ 252c --| 2514   251c
   if (head == NULL)
     return;
   while (head) {
@@ -39,18 +48,23 @@ void printTree(struct Node *head, int size) {
       if (head->child->brother) {
         // u252c ┬
         printf("\u2500\u252c\u2500");
+        flag[size + 2] = 'a';
+
       } else
         printf("\u2500\u2500\u2500");
-      size = size + strlen(head->names) + 3;
-      printf("%d\n", size);
-      printTree(head->child, size);
+
+      int sizein = size + strlen(head->names) + 3;
+      printTree(head->child, sizein, flag);
     }
     head = head->brother;
     if (head) {
       printf("\n");
-      printf("%d\n", size);
+
       for (int i = 0; i < size - 2; i++) {
-        printf(" ");
+        if (flag[i] == 'a')
+          printf("\u2502");
+        else
+          printf(" ");
       }
       if (head->brother)
         // u251c ├
@@ -59,60 +73,52 @@ void printTree(struct Node *head, int size) {
       else
         printf("\u2514\u2500");
     }
-
-    //   printf("%s", head->names);
-    //   if (head->child) {
-    //     if (head->child->brother)
-    //       printf("\u2500\u252c\u2500");
-    //     else
-    //       printf("\u2500\u2500\u2500");
-    //     size = strlen(head->names) + 3 + size;
-    //     // printf("%d\n", size);
-    //     printTree(head->child, size);
-    //   }
-    //
-    //   head = head->brother;
-    //   if (head) {
-    //     printf("\n");
-    //     // if (head->brother)
-    //     //   printf("11111");
-    //     for (int i = 0; i < size; i++) {
-    //       printf("\u2500");
-    //     }
-    //   }
   }
 }
-int main(int argc, char *argv[]) {
-  struct Node *second = buildNode("2", 2, NULL, NULL);
-  struct Node *first = buildNode("1", 1, NULL, NULL);
+// int main(int argc, char *argv[]) {
+//   struct Node *second = buildNode("2", 2, NULL, NULL);
+//   struct Node *first = buildNode("1", 1, NULL, NULL);
+//
+//   struct Node *third = buildNode("3", 3, NULL, NULL);
+//   struct Node *fourth = buildNode("5", 5, NULL, NULL);
+//
+//   struct Node *fith = buildNode("4", 4, NULL, NULL);
+//
+//   struct Node *six = buildNode("6", 6, NULL, NULL);
+//   struct Node *seven = buildNode("7", 7, NULL, NULL);
+//   struct Node *eight = buildNode("8", 8, NULL, NULL);
+//   struct Node *e = buildNode("9", 8, NULL, NULL);
+//
+//   struct Node *e1 = buildNode("18", 8, NULL, NULL);
+//   struct Node *e2 = buildNode("28", 8, NULL, NULL);
+//   struct Node *e3 = buildNode("38", 8, NULL, NULL);
+//
+//   first->child = second;
+//   second->child = third;
+//   third->brother = fourth;
+//   third->child = six;
+//   six->child = seven;
+//
+//   seven->brother = fith;
+//   fith->brother = e;
+//   // fourth->brother = fith;
+//   six->brother = e1;
+//   e1->brother = e2;
+//   e2->brother = e3;
+//
+//   // third->child = six;
+//   // six->child = second;
+//   // six->brother = eight;
+//   char *flag = (char *)malloc(sizeof(char));
+//   printTree(first, 0, flag);
+// }
 
-  struct Node *third = buildNode("3", 3, NULL, NULL);
-  struct Node *fourth = buildNode("5", 5, NULL, NULL);
-
-  struct Node *fith = buildNode("4", 4, NULL, NULL);
-
-  struct Node *six = buildNode("6", 6, NULL, NULL);
-  struct Node *seven = buildNode("7", 7, NULL, NULL);
-  struct Node *eight = buildNode("8", 8, NULL, NULL);
-  first->child = second;
-  second->child = third;
-  third->brother = fourth;
-  third->child = six;
-  six->child = seven;
-
-  fourth->brother = fith;
-
-  // third->child = six;
-  // six->child = second;
-  // six->brother = eight;
-  printTree(first, 0);
-}
-
-void getPPid(FILE *fp) {
+struct status *getPPid(FILE *fp) {
   char line[64];
-  pid_t ppid = -1;
+  pid_t ppid = -1, pid = -1;
   int i, j, k, l = -1;
   char *names;
+  struct status *stus;
 
   names = (char *)malloc(60 * sizeof(char));
 
@@ -121,12 +127,25 @@ void getPPid(FILE *fp) {
     if (strncmp(line, "PPid:", 5) == 0) {
       sscanf(line, "PPid:%u", &ppid);
     }
+    if (strncmp(line, "Pid:", 4) == 0)
+      sscanf(line, "Pid:%u", &pid);
     if (strncmp(line, "Name:", 5) == 0) {
       sscanf(line, "Name:%s", names);
       l = 0;
     }
-    if (l == 0 && ppid != -1) {
+    if (l == 0 && ppid != -1 && pid != -1) {
+      stus = (struct status *)malloc(sizeof(struct status));
+      stus->ppid = ppid;
+      stus->pid = pid;
+      stus->names = names;
+      return stus;
     }
+  }
+  return NULL;
+}
+void insetNode(struct status *status) {
+
+  if (status->ppid == 0) {
   }
 }
 char *getcmdline(FILE *fp) {
@@ -138,47 +157,48 @@ char *getcmdline(FILE *fp) {
   return NULL;
 }
 
-// int main(int argc, char *argv[]) {
-//   struct dirent *dt;
-//   DIR *dirs;
-//   char *endptr;
-//   pid_t pid;
-//   uid_t uid;
-//   char str[50] = "/proc/";
-//   FILE *fp;
-//   char *names;
-//   // struct NameAndUid nad;
-//
-//   names = (char *)malloc(40 * (sizeof(char)));
-//   if (names == NULL) {
-//     printf("malloc error\n");
-//     exit(EXIT_FAILURE);
-//   }
-//
-//   // open /proc/ dir
-//   dirs = opendir("/proc");
-//   if (dirs == NULL) {
-//     printf("dir error\n");
-//     exit(EXIT_FAILURE);
-//   }
-//   while ((dt = readdir(dirs)) != NULL) {
-//     pid = strtol(dt->d_name, &endptr, 10);
-//
-//     // chech if dt_type is dir
-//     if (dt->d_type == DT_DIR && *endptr == '\0') {
-//       // cat string
-//       strcat(str, dt->d_name);
-//       strcat(str, "/status");
-//
-//       // open file
-//       fp = fopen(str, "r");
-//       if (fp == NULL) {
-//         printf("file open error");
-//         exit(EXIT_FAILURE);
-//       }
-//       // init str
-//       strcpy(str, "/proc/");
-//     }
-//   }
-//   exit(EXIT_SUCCESS);
-// }
+int main(int argc, char *argv[]) {
+  struct dirent *dt;
+  DIR *dirs;
+  char *endptr;
+  pid_t pid;
+  uid_t uid;
+  char str[50] = "/proc/";
+  FILE *fp;
+  char *names;
+  struct status *stus;
+
+  names = (char *)malloc(40 * (sizeof(char)));
+  if (names == NULL) {
+    printf("malloc error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // open /proc/ dir
+  dirs = opendir("/proc");
+  if (dirs == NULL) {
+    printf("dir error\n");
+    exit(EXIT_FAILURE);
+  }
+  while ((dt = readdir(dirs)) != NULL) {
+    strtol(dt->d_name, &endptr, 10);
+
+    // chech if dt_type is dir
+    if (dt->d_type == DT_DIR && *endptr == '\0') {
+      // cat string
+      strcat(str, dt->d_name);
+      strcat(str, "/status");
+
+      // open file
+      fp = fopen(str, "r");
+      if (fp == NULL) {
+        printf("file open error");
+        exit(EXIT_FAILURE);
+      }
+      // init str
+      strcpy(str, "/proc/");
+      stus = getPPid(fp);
+    }
+  }
+  exit(EXIT_SUCCESS);
+}
