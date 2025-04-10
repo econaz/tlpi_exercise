3e37d4c9711efed41f15157aa1bce11b09ef89f7
econaz <econazx@gmail.com>
2025-04-10 21:40:40 +0800

    complete 13_5.c

--- chapter13/13_5.c
+++ chapter13/13_5.c
@@ -1,61 +1,77 @@
 #include <fcntl.h>
+#include <math.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-void read_line(char *buf, size_t readNum) {
+#define BUF_SIZE 4096
 
-  int i;
-  int line = 0;
-  char *cur;
-  for (i = readNum - 1; i >= 0; i--) {
+int read_tail(char *file, int line) {
+  int pos = 0, fd, readline, i;
+  off_t filesize, readsize;
+  ssize_t readnum;
+  char buf[BUF_SIZE];
 
-    if (buf[i] == '\n') {
-      line++;
+  fd = open(file, O_RDONLY);
+  if (fd == -1) {
+    printf("open error");
+    return -1;
+  }
+
+  // get file size and position file end
+  filesize = lseek(fd, 0, SEEK_END);
+  if (filesize == -1) {
+    printf("lseek error1");
+    return -1;
+  }
+
+  while (abs(pos) < filesize && readline < line + 1) {
+
+    // cal remaind file size
+    readsize =
+        (filesize - abs(pos) > BUF_SIZE) ? BUF_SIZE : filesize - abs(pos);
+
+    if (lseek(fd, -readsize, SEEK_CUR) == -1) {
+      printf("lseek error2");
+      exit(EXIT_FAILURE);
     }
-  }
-}
-void readLine(int fd, int line) {
 
-  size_t readNum;
-  char buf[4096];
-  off_t size = -4096;
-  int curline = 0;
-  char *curptr;
+    if ((readnum = read(fd, buf, readsize)) == -1) {
+      printf("read error");
+      exit(EXIT_FAILURE);
+    }
 
-  if (lseek(fd, size, SEEK_END) == -1) {
-    printf("lseek error!");
-    return;
-  }
-
-  while ((readNum = read(fd, buf, size)) != -1) {
-    for (int i = readNum - 1; i >= 0; i--) {
+    for (i = readnum - 1; i >= 0; i--) {
       if (buf[i] == '\n')
-        curline++;
-      if (curline == line)
+        readline++;
+      // read line + one,the posistion add one;
+      if (readline == line + 1) {
+        pos = pos - (readnum - i) + 1;
         break;
+      }
     }
-    if (curline == line)
-      break;
-
-    if (lseek(fd, size, SEEK_CUR) == -1) {
-      printf("lseek error!");
-      return;
-    };
+    if (readline < line + 1) {
+      pos -= readnum;
+    }
   }
+  if (lseek(fd, pos, SEEK_END) == -1) {
+    printf("lseek error3");
+    return -1;
+  }
+  while ((readnum = read(fd, buf, BUF_SIZE)) > 0) {
+    if (write(STDOUT_FILENO, buf, readnum) == -1) {
+      printf("write error");
+      return -1;
+    }
+  }
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
-  int fd;
-  off_t offset;
   int line = 10;
   char *file;
-  size_t readnum;
-  int filesize;
-  size_t bufsize = 4096;
-  char buf[4096];
 
   if (argc == 4 && strcmp(argv[1], "-n") == 0) {
     line = atoi(argv[2]);
@@ -67,24 +83,9 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  fd = open(file, O_RDONLY);
-
-  filesize = lseek(fd, 0, SEEK_END);
-  if (filesize == -1) {
-    printf("lseek error");
+  if (read_tail(file, line) == -1) {
+    printf("read_tail error!");
     exit(EXIT_FAILURE);
   }
-
-  bufsize = (filesize > bufsize) ? bufsize : filesize;
-  if (lseek(fd, -bufsize, SEEK_END) == -1) {
-    printf("lseek error");
-    exit(EXIT_FAILURE);
-  }
-
-  if ((readnum = read(fd, buf, bufsize)) == -1) {
-    printf("read error");
-    exit(EXIT_FAILURE);
-  }
-
-  return EXIT_SUCCESS;
+  exit(EXIT_SUCCESS);
 }
