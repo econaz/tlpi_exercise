db31ccd9f16dbc2869864943b63acd0c2abe7a96
econaz <econazx@gmail.com>
2025-04-11 19:59:05 +0800

    add chapter14

---
+++ chapter14/14_1.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+void creatFile(const char *dir, int num) {
+
+  char filename[20];
+  FILE *fp;
+  int i;
+
+  for (i = 0; i < num; i++) {
+  }
+}
+
+int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
