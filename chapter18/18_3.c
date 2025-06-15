5b6ffcd19730381c38d7df768a55152785c1a7df
econaz <econazx@gmail.com>
2025-06-15 20:56:59 +0800

    add 18_3.c fix

--- chapter18/18_3.c
+++ chapter18/18_3.c
@@ -16,19 +16,35 @@ char *myrealpath(const char *name, char *resolved) {
 
   struct stat statbuf;
   char buf[BUF_SIZE];
+  char bufN[BUF_SIZE];
   ssize_t numBytes;
+  int fd;
 
-  if (lstat(name, &statbuf) == -1)
+  if (stat(name, &statbuf) == -1)
     return NULL;
 
   numBytes = readlink(name, buf, BUF_SIZE - 1);
   if (numBytes == -1) {
-    printf("error readlink\n");
-    exit(EXIT_FAILURE);
+    printf("error readlink");
+    return NULL;
   }
-
-  buf[numBytes] = '\0';
-  printf("%s\n", buf);
+  if (buf[0] == '/') {
+    snprintf(resolved, numBytes, "%s", buf);
+    return resolved;
+  } else {
+    snprintf(bufN, BUF_SIZE, "%s", name);
+    fd = open(dirname(bufN), O_RDONLY);
+    if (fd == -1)
+      return NULL;
+    if (fchdir(fd) == -1)
+      return NULL;
+    if (!getcwd(resolved, BUF_SIZE))
+      return NULL;
+    strcat(resolved, "/");
+    strcat(resolved, buf);
+    close(fd);
+    return resolved;
+  }
   return NULL;
 }
 
@@ -36,4 +52,5 @@ int main(int argc, char *argv[]) {
 
   char str[BUF_SIZE];
   myrealpath(argv[1], str);
+  printf("%s-----> %s", argv[1], str);
 }
