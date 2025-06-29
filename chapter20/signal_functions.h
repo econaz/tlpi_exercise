61e2ec18f7d933b85668407b8e735bc53e96e1ac
econaz <econazx@gmail.com>
2025-06-29 20:32:19 +0800

    add chapter20

---
+++ chapter20/signal_functions.h
@@ -0,0 +1,7 @@
+
+#include <signal.h>
+#include <stdio.h>
+
+void printSigset(FILE *of, const char *prefix, const sigset_t *sigset);
+int printSigMask(FILE *of, const char *msg);
+int printPendingSigs(FILE *of, const char *msg);
