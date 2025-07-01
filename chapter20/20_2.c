b35c66e2c7a7e55374ddb5ea3c20496e76c6c4a3
econaz <econazx@gmail.com>
2025-07-01 20:26:06 +0800

    complete 20_2.c

--- chapter20/20_2.c
+++ chapter20/20_2.c
@@ -1,3 +1,23 @@
-#define _GNU_SOURCE
-#include "tlpi_hdr.h"
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  struct sigaction act;
+
+  printf("pid is %ld\n", (long)getpid());
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = 0;
+  act.sa_handler = SIG_IGN;
+  if (sigaction(SIGTERM, &act, NULL) == -1) {
+
+    printf("sigaction");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+  }
+
+  exit(EXIT_SUCCESS);
+}
