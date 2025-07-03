6138ea21417cd12d8971db21d931891e6e3d34e7
econaz <econazx@gmail.com>
2025-07-03 20:40:55 +0800

    complete 20_3_2.c

--- chapter20/20_3_2.c
+++ chapter20/20_3_2.c
@@ -1,13 +1,18 @@
+
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-static void handler(int sig) { printf("first %d\n", sig); }
+static void handler(int sig) {
+  printf("in\n");
+  sleep(10);
+  printf("out\n");
+}
 int main(int argc, char *argv[]) {
   struct sigaction act;
-  act.sa_flags = SA_RESETHAND;
+  act.sa_flags = SA_NODEFER;
   act.sa_handler = handler;
   sigemptyset(&act.sa_mask);
 
