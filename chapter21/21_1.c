eca9d082e87608923459b1e4e5696abadba106fb
econaz <econazx@gmail.com>
2025-07-11 19:33:14 +0800

    complete 21_1.c

--- chapter21/21_1.c
+++ chapter21/21_1.c
@@ -1,11 +1,18 @@
 
+#include <bits/types/sigset_t.h>
 #include <signal.h>
+#include <string.h>
 
 void myAbort() {
 
   struct sigaction sa;
-
+  sigset_t mask;
+  sigaddset(&mask, SIGABRT);
+  sigprocmask(SIG_UNBLOCK, &mask, NULL);
   sa.sa_flags = 0;
-
   sigemptyset(&sa.sa_mask);
+
+  raise(SIGABRT);
+
+  sigaction(SIGABRT, &sa, NULL);
 }
