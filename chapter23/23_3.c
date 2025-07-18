5664fbe341779329f2635d98d0e4b83c3b720921
econaz <econazx@gmail.com>
2025-07-18 20:53:01 +0800

    modify 23_3.c

--- chapter23/23_3.c
+++ chapter23/23_3.c
@@ -1,4 +1,6 @@
 #include <bits/time.h>
+#include <bits/types/siginfo_t.h>
+#include <bits/types/sigset_t.h>
 #include <bits/types/timer_t.h>
 #include <csignal>
 #include <cstdlib>
@@ -14,12 +16,16 @@ int main(int argc, char *argv[]) {
   timer_t t;
   struct itimerspec val;
   struct sigaction sa;
+  int sig;
+  sigset_t sigs;
+  siginfo_t siginfo;
 
   sa.sa_flags = 0;
   sigemptyset(&sa.sa_mask);
   sa.sa_handler = handler;
 
-  if (sigaction(SIGINT, &sa, NULL) == -1)
+  sigaddset(&sigs, SIGALRM);
+  if (sigaction(SIGALRM, &sa, NULL) == -1)
     errExit("sigaction");
 
   if (timer_create(CLOCK_REALTIME, NULL, &t) == -1)
@@ -28,5 +34,11 @@ int main(int argc, char *argv[]) {
   if (timer_settime(&t, 0, &val, NULL) == -1)
     errExit("timer_settime");
 
+  for (;;) {
+    sig = sigwaitinfo(&sigs, &siginfo);
+    if (sig == -1)
+      errExit("sigwaitinfo");
+    get = siginfo.si_value.sival_int;
+  }
   return 0;
 }
