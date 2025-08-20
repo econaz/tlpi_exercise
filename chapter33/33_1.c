cf2c5aa408fbc10914d223316fa82aa802ba6407
econaz <econazx@gmail.com>
2025-08-20 19:35:35 +0800

    complete 33_2.c

--- chapter33/33_1.c
+++ chapter33/33_1.c
@@ -1,10 +1,10 @@
 
 
 #include "tlpi_hdr.h"
-#include <cstdio>
 #include <pthread.h>
 #include <signal.h>
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 static void printSigset(FILE *of, const char *prefix, const sigset_t *sigset) {
@@ -36,7 +36,20 @@ static int printPendingSigs(FILE *of, const char *msg) {
   return 0;
 }
 
-static void *threadFunc(void *arg) { printPendingSigs(stdin, NULL); }
+static void *threadFunc(void *arg) {
+  int s;
+  pthread_t tid = pthread_self();
+  printf("%lu got ", tid);
+  printPendingSigs(stdout, NULL);
+  return NULL;
+}
+
+void handler(int sig) {
+
+  pthread_t tid = pthread_self();
+  printf("Thread %lu catch signo = %d\n", tid, sig);
+  return;
+}
 
 int main(int argc, char *argv[]) {
   pthread_t phr, phr1;
@@ -45,7 +58,7 @@ int main(int argc, char *argv[]) {
   sigset_t block;
   sigfillset(&block);
 
-  s = pthread_sigmask(SIG_SETMASK, &block, NULL);
+  s = pthread_sigmask(SIG_BLOCK, &block, NULL);
   if (s != 0)
     errExitEN(s, "pthread_sigmask");
 
@@ -57,8 +70,12 @@ int main(int argc, char *argv[]) {
   if (s != 0)
     errExitEN(s, "pthread_create");
 
-  pthread_kill(phr, SIGINT);
+  pthread_kill(phr, SIGABRT);
   pthread_kill(phr1, SIGINT);
+
+  s = pthread_join(phr1, NULL);
+
+  s = pthread_join(phr, NULL);
 
   exit(EXIT_SUCCESS);
 }
