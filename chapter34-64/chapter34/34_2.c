6ec2ab74703dab1ec1ce67bcddd508a7b981fd5d
econaz <econazx@gmail.com>
2025-09-02 20:29:29 +0800

    add chapter34

---
+++ chapter34-64/chapter34/34_2.c
@@ -0,0 +1,42 @@
+
+#include "tlpi_hdr.h"
+#include <cerrno>
+#include <error_functions.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  pid_t gpid;
+  pid_t pid = fork();
+  switch (pid) {
+  case -1:
+    errExit("fork");
+  case 0:
+    sleep(10);
+    execl("/usr/bin/sleep", "sleep", "10", (char *)NULL);
+    _exit(EXIT_SUCCESS);
+  default:
+    break;
+  }
+
+  if ((gpid = getpgid(pid)) == -1)
+    errExit("getpgid");
+
+  printf("before pgid = %ld\n", (long)gpid);
+
+  if (setpgid(pid, pid) == -1) {
+
+    printf("cant modify pgid\n");
+  }
+
+  sleep(4);
+  if (setpgid(pid, pid) == -1)
+    errExit("setpgid");
+
+  if ((gpid = getpgid(pid)) == -1)
+    errExit("getpgid");
+
+  printf("After pgid = %ld", (long)gpid);
+  return EXIT_SUCCESS;
+}
