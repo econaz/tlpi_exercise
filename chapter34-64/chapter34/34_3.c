88fafd49af46d73ce01cf37fe08e35ba0e0c6f40
econaz <econazx@gmail.com>
2025-09-04 20:17:38 +0800

    add 34_4.c

--- chapter34-64/chapter34/34_3.c
+++ chapter34-64/chapter34/34_3.c
@@ -15,8 +15,23 @@ int main(int argc, char *argv[]) {
   case 0:
 
     if ((sid = getsid(pid)) == -1)
-      errExit("getsid");
+      errExit("getsid\n");
     printf("before sid = %ld\n", (long)sid);
+
+    if (setpgid(0, 0) == -1)
+      errExit("setpgid");
+    printf("first process\n");
+
+    if (setsid() == -1)
+      errExit("setsid");
+
+    if ((sid = getsid(pid)) == -1)
+      errExit("getsid\n");
+
+    printf("after sid = %ld\n", (long)sid);
+
+    _exit(EXIT_SUCCESS);
   }
+
   return EXIT_SUCCESS;
 }
