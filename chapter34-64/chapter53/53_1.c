0d81f4733f7c43fad9ae8e1374f9909be84882cf
econaz <econazx@gmail.com>
2026-03-22 16:21:17 +0800

    fix 54_1_reader.c

--- chapter34-64/chapter53/53_1.c
+++ chapter34-64/chapter53/53_1.c
@@ -1,23 +1,20 @@
-#include <pthread.h>
-#include <fcntl.h>
-#include <semaphore.h>
 #include "tlpi_hdr.h"
-
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 #define BUF_SIZE 4096
-
 
 static char buffer[BUF_SIZE];
 static sem_t wSem;
 static sem_t rSem;
 static volatile ssize_t num = 0;
 
+void *readFunc(void *arg) {
 
-void *readFunc(void *arg){
+  int xfrs, bytes;
 
-  int xfrs,bytes;
-
-  for (xfrs = 0,bytes = 0;;xfrs++){
+  for (xfrs = 0, bytes = 0;; xfrs++) {
 
     if (sem_wait(&rSem) == -1)
       errExit("sem_wait");
@@ -29,42 +26,38 @@ void *readFunc(void *arg){
     if (write(STDOUT_FILENO, buffer, num) != num)
       fatal("partial/failed write");
 
-   if (sem_post(&wSem) == -1)
+    if (sem_post(&wSem) == -1)
       errExit("sem_post");
   }
 
-
   if (sem_post(&wSem) == -1)
     errExit("sem_wait");
-  
-  fprintf(stderr, "Recevie %d bytes (%d xfrs)\n",bytes,xfrs);
-  
-  return NULL;
 
+  fprintf(stderr, "Recevie %d bytes (%d xfrs)\n", bytes, xfrs);
+
+  return NULL;
 }
 
-void *writeFunc(void *arg){
+void *writeFunc(void *arg) {
 
-  int xfrs,bytes;
+  int xfrs, bytes;
 
-  for (xfrs = 0,bytes = 0;;xfrs++,bytes+=num){
+  for (xfrs = 0, bytes = 0;; xfrs++, bytes += num) {
     if (sem_wait(&wSem) == -1)
-     errExit("sem_wait");
+      errExit("sem_wait");
 
     num = read(STDIN_FILENO, buffer, BUF_SIZE);
     if (num == -1)
       errExit("read");
-
 
     if (sem_post(&rSem) == -1)
       errExit("sem_post");
 
     if (num == 0)
       break;
-
   }
-  
-  fprintf(stderr, "Sent %d bytes (%d xfrs)\n",bytes,xfrs);
+
+  fprintf(stderr, "Sent %d bytes (%d xfrs)\n", bytes, xfrs);
 
   if (sem_wait(&rSem) == -1)
     errExit("sem_post");
@@ -72,16 +65,15 @@ void *writeFunc(void *arg){
   return NULL;
 }
 
-int main(int argc,char *arg[]){
+int main(int argc, char *arg[]) {
 
-  pthread_t rThread,wThread;
+  pthread_t rThread, wThread;
   int s;
 
-  
   if (sem_init(&wSem, 0, 1) == -1)
     errExit("sem_init");
 
-  if (sem_init(&rSem,0,0) == -1)
+  if (sem_init(&rSem, 0, 0) == -1)
     errExit("sem_init");
 
   s = pthread_create(&wThread, NULL, writeFunc, NULL);
@@ -98,7 +90,7 @@ int main(int argc,char *arg[]){
 
   s = pthread_join(rThread, NULL);
   if (s != 0)
-    errExitEN(s,"pthread_join");
- 
+    errExitEN(s, "pthread_join");
+
   exit(EXIT_SUCCESS);
 }
--- chapter34-64/chapter53/53_4_posix.c
+++ chapter34-64/chapter53/53_4_posix.c
@@ -1,38 +1,34 @@
-#include <semaphore.h>
-#include <pthread.h>
-#include <sys/time.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "tlpi_hdr.h"
-
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #define THREAD_NUM 20
 #define LOOPS 100000
 
-
 static sem_t sem;
 
+void *threadFunc(void *arg) {
 
-void *threadFunc(void *arg){
-
-  for (int i = 0;i < LOOPS;i++){
-    //todo
+  for (int i = 0; i < LOOPS; i++) {
+    // todo
     sem_wait(&sem);
     sem_post(&sem);
-    
   }
   return NULL;
 }
 
-int main(int argc,char *argv[]){
+int main(int argc, char *argv[]) {
 
   pthread_t thr[THREAD_NUM];
   int pthreadNums;
-  struct timeval startTime,endTime;
+  struct timeval startTime, endTime;
   double timeUse;
 
-  if (argc != 2 || strcmp(argv[1],"--help") == 0)
-    usageErr("%s usage: thread-nums\n",argv[0]);
+  if (argc != 2 || strcmp(argv[1], "--help") == 0)
+    usageErr("%s usage: thread-nums\n", argv[0]);
 
   pthreadNums = atoi(argv[1]);
 
@@ -40,19 +36,18 @@ int main(int argc,char *argv[]){
 
   gettimeofday(&startTime, NULL);
 
-  for (int i = 0;i< pthreadNums;i++)
+  for (int i = 0; i < pthreadNums; i++)
     if (pthread_create(&thr[i], NULL, threadFunc, NULL) != 0)
       errExit("pthread_create");
 
-  for (int i = 0;i<pthreadNums;i++)
+  for (int i = 0; i < pthreadNums; i++)
     if (pthread_join(thr[i], NULL) != 0)
       errExit("pthread_join");
 
   gettimeofday(&endTime, NULL);
 
-  timeUse = (endTime.tv_sec - startTime.tv_sec) + (endTime.tv_usec - startTime.tv_usec) / 1000000.00;
+  timeUse = (endTime.tv_sec - startTime.tv_sec) +
+            (endTime.tv_usec - startTime.tv_usec) / 1000000.00;
 
-  printf("use time = %lf\n",timeUse);
- 
-
+  printf("use time = %lf\n", timeUse);
 }
